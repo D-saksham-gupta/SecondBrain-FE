19fd16f32f1910a2a98d5540659dc327123f6b54
Saksham Gupta <sakshamprem2003@gmail.com>
2024-12-06 18:47:10 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "http://localhost:3000";
+export const BACKEND_URL = "https://secondbrain-backend-8srz.onrender.com";
 export const FRONTEND_URL = "http://localhost:5173";
