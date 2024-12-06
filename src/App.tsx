5174c8842a35419b46550d7418e7ffe98c2e8b66
Saksham Gupta <sakshamprem2003@gmail.com>
2024-12-06 23:51:45 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/signup" element={<Signup />} />
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={<Signin />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
